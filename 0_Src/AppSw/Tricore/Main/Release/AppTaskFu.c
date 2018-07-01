f7091f310f420eacfedba5a8e52d538fce82d3d5
KIM MINKYU <kimmin7894@gmail.com>
2018-07-01 16:33:25 +0900

    스케쥴러에 함수 동작

--- 0_Src/AppSw/Tricore/Main/Release/AppTaskFu.c
+++ 0_Src/AppSw/Tricore/Main/Release/AppTaskFu.c
@@ -48,6 +48,8 @@ void appTaskfu_10ms(void)
 	if(task_cnt_10m%2 == 0){
 		BasicLineScan_run();
 		InfineonRacer_detectLane();
+		InfineonRacer_Avoid(task_cnt_1m);
+		InfinedonRacer_AEB();
 		BasicPort_run();
 		BasicGtmTom_run();
 		BasicVadcBgScan_run();
